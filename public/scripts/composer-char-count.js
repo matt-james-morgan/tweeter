e03ef54ed629b586d0b3f3fe6e7cb5cf2fb72b02
Matt Morgan <matt.james.morgan97@gmail.com>
2023-12-07 21:46:36 +0000

    add comments to code

--- public/scripts/composer-char-count.js
+++ public/scripts/composer-char-count.js
@@ -1,17 +1,19 @@
 $(document).ready(function(){
 
   $("#tweet-text").on("input",function(){
+    //Get values of user input
     let userTweet = $(this).val();
 
+    //determines how many chars are left
     let charRemaining = 140 - userTweet.length;
 
+    //navigates up and down dom tree to select output char count
     let form = $(this).parent();
-
     let charCount = $(form).find(".counter");
 
+    //if count is over 140 set the color of output to red
     if (charRemaining >= 0) {
       charCount.text(charRemaining);
-      // Reset the text color to its default value
     } else {
       charCount.text(charRemaining);
       charCount.css("color", "red");
