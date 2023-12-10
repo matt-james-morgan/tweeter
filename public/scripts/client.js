1477fd052e4e98b9cedc1c638b4686c4df4a77f0
matt-james-morgan <matt.james.morgan97@gmail.com>
2023-12-10 12:29:59 -0500

    adds logic for requesting data from server and renders data

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,33 +3,12 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+
+//waits for document to load before exacuting JS logic
 $(document).ready(()=>{
-    const data = [
-        {
-          "user": {
-            "name": "Newton",
-            "avatars": "https://i.imgur.com/73hZDYK.png"
-            ,
-            "handle": "@SirIsaac"
-          },
-          "content": {
-            "text": "If I have seen further it is by standing on the shoulders of giants"
-          },
-          "created_at": 1461116232227
-        },
-        {
-          "user": {
-            "name": "Descartes",
-            "avatars": "https://i.imgur.com/nlhLi3I.png",
-            "handle": "@rd" },
-          "content": {
-            "text": "Je pense , donc je suis"
-          },
-          "created_at": 1461113959088
-        }
-      ]
     
     
+    //looops through an array of data and appends new html to index
     const renderTweets = (tweets) => {
         for(let tweet of tweets){
             let $tweet = createTweet(tweet);
@@ -37,6 +16,7 @@ $(document).ready(()=>{
         }
     }
     
+    //creates html to render new tweet
     const createTweet = (tweetObj) =>{
         let $tweet;
       
@@ -52,7 +32,7 @@ $(document).ready(()=>{
       </header>
       <textarea name="tweet">${tweetObj.content.text}</textarea>
       <footer>
-        <p>${tweetObj.created_at}</p>
+        <p>${timeago.format(tweetObj.created_at)}</p>
         <div class="tweet-icons-container">
           <i class="fa-solid fa-flag"></i>
         <i class="fa-solid fa-repeat"></i>
@@ -63,8 +43,8 @@ $(document).ready(()=>{
         return $tweet;
     }
     
-    renderTweets(data);
-    
+   
+    //adds event listner on the submit of a new tweet and posts data to server
     $(".new-tweet").submit((e)=>{
         e.preventDefault();
         const newText = $("#tweet-text").serialize();
@@ -78,7 +58,17 @@ $(document).ready(()=>{
             })
     })
     
+    //uses ajax to request data from server and then calls renderTweets upon success
+    const loadTweets = () =>{
+         $.ajax({
+            url: "/tweets",
+            type: "GET",
+        }).then((res)=>{
+            renderTweets(res);
+        }).catch((err)=>{
+            console.log(err)
+        })
+    }
     
-    
-    
+    loadTweets();
 })
